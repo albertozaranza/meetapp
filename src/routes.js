bfc841704a20e77f3945a69030ca785fd03567a7
albertozaranza <albertozaranza@outlook.com>
2019-06-30 03:43:02 -0300

    Criação do loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello alb' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Alberto Zaranza',
+    email: 'albertozaranza@outlook.com',
+    password_hash: '123456',
+  });
+  return res.json({ user });
 });
 
 export default routes;
