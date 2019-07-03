b568209098263c064a36f9be73f5d4418ad3546c
albertozaranza <albertozaranza@outlook.com>
2019-07-03 02:11:55 -0300

    Listagem de Meetups do usuário logado

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
+import OrganizingController from './app/controllers/OrganizingController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -22,6 +23,8 @@ routes.put('/users', UserController.update);
 routes.get('/meetups', MeetupController.index);
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
+
+routes.get('/organizing', OrganizingController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
